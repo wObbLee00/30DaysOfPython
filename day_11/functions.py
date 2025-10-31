1cd5c8a9da1245401a75a48e20124d2447ba3da7
wObbLee <wabilbutt786@gmail.com>
2025-10-31 22:50:28 +0500

    Little work on functions.py. Just tired and no motivation so didnt finish

---
+++ day_11/functions.py
@@ -0,0 +1,54 @@
+#excercise 1
+#q1
+def add_two_numbers(num1,num2):
+    return num1+num2
+print(add_two_numbers(2,3))
+#q2
+def area_of_circle(r,pie=3.14):
+    return pie*r*r
+print(area_of_circle(10))
+#q2
+
+#q3
+
+#q4
+
+#q5
+
+#q6
+
+#q7
+
+#q8
+
+#q9
+
+#q10
+
+#q11
+
+#q12
+
+#q13
+
+#q14
+
+#q15
+
+#excercise 2
+#q1
+
+#q2
+
+#q3
+
+#excercise 3
+#q1
+
+#q2
+
+#q3
+
+#q4
+
+#q5
